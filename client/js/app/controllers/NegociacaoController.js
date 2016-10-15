6927cf666f97c2d8e385cac165e689999c8f26f4
Manoel Motoso <Manoel Motoso>
2016-10-15 17:02:06 -0300

    Adicionado class Mensagem e fetatorado o codigo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,85 +1,87 @@
-class NegociacaoController{
-	constructor(){
-		/*Atribuindo a funcao do querySelecto a varuaval dolar*/
-		let $ = document.querySelector.bind(document);
-		this._inputData =$("#data");
-		this._inputQuantidade = $("#quantidade");
-		this._inputValor = $("#valor");
-		let self = this;
-		/*Passando uma arraw functin que ira carregar as mudancas
-		 *na view toda vez que for adicionado ou removido uma negociacao
-		 */
-		this._listaNegociacoes = new Proxy(new ListaNegociacoes(),
-		{
-			get(target, prop, reciver){
-				if(['adiciona','apagar'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-					return function(){
-						Reflect.apply(target[prop], target, arguments);
-						self._negociacoesView.update(target);
-					};
-				}
-				
-				return Reflect.get(target, prop, reciver);
-			}
-		}
-		);
-		
-		/*Instanciando a classe NegociacoesView e uma MenssagemView passando como parametro o 
-		 *elemento que recebera o template string respectivo de cada uma.
-		 */
-		this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-		this._negociacoesView.update(this._listaNegociacoes);
-		this._menssagemView = new MenssagemView($('#menssagemView'));
-		this._menssagemView.update();
-	}
+class NegociacaoController {
+    constructor() {
+        /*Atribuindo a funcao do querySelecto a varuaval dolar*/
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+        let self = this;
 
-	adiciona(event){
-		/*Desbilita o submit do botao Incluir*/
-		event.preventDefault();
-	
-		/*criando uma instacia da classe Negociacao e 
-		 *passando os paremetros de uma nova negociacao
-		 */
-		let negociacao = new Negociacao(
-			DataHelper.textoParaData(this._inputData.value),
-			this._inputQuantidade.value,
-			this._inputValor.value
-		);
-		
-		this._listaNegociacoes.adiciona(negociacao);	
-		this._limpaFormulario();
-		this._negociacoesView.update(this._listaNegociacoes);
-		this._menssagemView.update("Sucesso");
-	}
-	/** Limpa o formulario a cada nova negociacao incluida */
-    _limpaFormulario(){
-	  this._inputData.value="";
-	  this._inputQuantidade.value=0;
-	  this._inputValor.value=0.0;
-	  this._inputData.focus();
+        /**/
+    
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, reciver) {
+                if (['adiciona', 'apagar'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                    return function() {
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    };
+                }
+                return Reflect.get(target, prop, reciver);
+            }
+        });
+
+        /*Instanciando a classe NegociacoesView e uma MensagemView passando como parametro o 
+         *elemento que recebera o template string respectivo de cada uma.
+         */
+   	    this._mensagem = new Mensagem();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagemView.update(this._mensagem);
     }
 
-	negociacoes(){
-		
-		let negociacaoService = new NegociacaoService();
+    adiciona(event) {
+            /*Desbilita o submit do botao Incluir*/
+            event.preventDefault();
+            /*criando uma instacia da classe Negociacao e 
+             *passando os paremetros de uma nova negociacao
+             */
+            let negociacao = new Negociacao(
+                DataHelper.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.value
+            );
+
+            this._listaNegociacoes.adiciona(negociacao);
+            this._limpaFormulario();
+            this._negociacoesView.update(this._listaNegociacoes);
+            this._mensagem.texto = "Negociação adicionada com sucesso.";
+            this._mensagemView.update(this._mensagem);
+        }
+        /*Limpa os campos do formulario a cada nova negociaçâo incluida */
+    _limpaFormulario() {
+
+        this._inputData.value = "";
+        this._inputQuantidade.value = 0;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    negociacoes() {
+
+        let negociacaoService = new NegociacaoService();
         /*Executa  Promises em sequencia, so executa a seguinte caso
-		 *a anterior tenha sido finalizada 
-		 */
+         *a anterior tenha sido finalizada 
+         */
         Promise.all([
-					negociacaoService.negociacoesSemanaAtual(),
-					negociacaoService.negociacoesSemanaAnterior(),
-					negociacaoService.negociacoesSemanaRetrasada()
-        ])
-        .then(negociacoes => negociacoes
-        	.reduce((arrayAchatado, array) => arrayAchatado.concat(array))
-        	.forEach(negociacao => {
-				this._listaNegociacoes.adiciona(negociacao);
-				
-				this._menssagemView.update("Importado");
-				}))
-		.catch(erro => this._menssagemView.update(erro));
-	}
-	apagar(){
-		this._listaNegociacoes.apagar();
-	}
+                negociacaoService.negociacoesSemanaAtual(),
+                negociacaoService.negociacoesSemanaAnterior(),
+                negociacaoService.negociacoesSemanaRetrasada()
+            ])
+            .then(negociacoes => negociacoes
+                .reduce((arrayAchatado, array) => arrayAchatado.concat(array))
+                .forEach(negociacao => {
+                    this._listaNegociacoes.adiciona(negociacao);
+                    this._mensagem.texto = "Negociacoes importadas com sucesso.";
+          		    this._mensagemView.update(this._mensagem);
+                }))
+            .catch(erro => this._mensagemView.update(this._mensagem.texto = erro));
+    }
+    apagar() {
+
+        this._listaNegociacoes.apagar();
+        this._mensagem.texto = "Negociacoes apagadas com sucesso.";
+        this._mensagemView.update(this._mensagem);
+    }
 }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,40 +1,38 @@
 class NegociacoesView extends View {
     constructor(element) {
         super(element);
-		
+
     }
-    template(model){
-        return `
-		<table class="table table-hover table-bordered">
-            <thead>
-                <tr class="active">
-                    <th>DATA</th>
-                    <th>QUANTIDADE</th>
-                    <th>VALOR</th>
-                    <th>VOLUME</th>
-                </tr>
-            </thead>
-            <tbody>
-			${model.negociacoes.map(n =>{
-					
-					return `
-						<tr>
-                           <td>${DataHelper.dataParaTexto(n.data)}</td>
-                           <td>${n.quantidade}</td>
-                           <td>${n.valor}</td>
-                          <td>${n.volume}</td>
-                         </tr>
-						`	
-				}).join('')
-			}
-            </tbody>
-			<tfoot>
-			<td colspan="3">Total</td>
-			<td class="active">
-			${model.negociacoes.reduce((total, n)=> total + n.volume,0.0)}
-			</td>
-			</tfoot>
-        </table>
-		`;
-   }
+    template(model) {
+            return `
+             <table class="table table-hover table-bordered">
+                          <thead>
+                              <tr class="active">
+                                  <th>DATA</th>
+                                  <th>QUANTIDADE</th>
+                                  <th>VALOR</th>
+                                  <th>VOLUME</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                            ${model.negociacoes.map(n => 
+                              `<tr>
+                                   <td>${DataHelper.dataParaTexto(n.data)}</td>
+                                   <td>${n.quantidade}</td>
+                                   <td>${n.valor}</td>
+                                   <td>${n.volume}</td>
+                              </tr>` 
+                            ).join('')
+                           }
+                          </tbody>
+                          <tfoot>
+                            <td colspan="3">Total</td>
+                            <td class="active">
+                                ${model.negociacoes.reduce((total, n)=> total + n.volume,0.0)}
+                            </td>
+                          </tfoot>
+                  </table>
+                  `
+        ;
+    }
 }
