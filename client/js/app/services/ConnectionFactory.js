01a28456597e537cac9f76c23f06746c918d8849
Manoel Motoso <manoel.s.motoso@gmail.com>
2016-10-20 18:27:23 -0200

    inplementando connection factory

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,48 +1,64 @@
-let dbName = 'aluraframe';
-let version = 1;
-let stores = ['negociacoes']
 
-class ConnectionFactory {
-	constrictor() {
-		throw new Error("Você não pode instanciar a classe ConnectionFactory pois seus metodos são staticos.");
+alert("hsjd");
+
+var ConnectionFactory = (function() {
+	var dbName = 'aluraframe';
+	var version = 2;
+	var stores = ['negociacoes'];
+	var connection=null;
+	var close = null;
+	return class ConnectionFactory{
+		constrictor() {
+			throw new Error("Você não pode instanciar a classe ConnectionFactory pois seus metodos são staticos.");
+		}
+
+		static getConnection() {
+			return new Promise((resolve, reject) => {
+			var openRequest = window.indexedDB.open(dbName, version);
+
+			openRequest.onupgradeneeded = e = > {
+			ConnectionFactory._createStores(e.target.result);
+			console.log('Object Store criado ou atualizado');
+			alert("Object store criado");
+		}
+
+		openRequest.onsuccess = e = > {
+		if (!connection)
+		{
+			connection = e.target.result;
+			alert("Object store instanciado");
+			//close = connection.close.bind(connection);
+			//connection.close = () => throw new Error("Voce nao pode fechar a conexao diretamente");
+
+		}
+		resolve(connection);
+
 	}
 
-	static getConnection() {
-		return new Promise((resolve, reject) => {
-			let openRequest = window.indexedDB.open(dbName, version);
-
-			openRequest.onupgradeneeded = e => {
-				ConnectionFactory._createStores(e.target.result)
-				console.log('Object Store criado ou atualizado');
-			}
-
-			openRequest.onsuccess = e => {
-				resolve(e.target.result)
-
-			}
-
-			openRequest.onerror = e => {
-				console.log(e.target.error);
-				reject(e.target.error)
-			}
-		});
-
+	openRequest.onerror = e = > {
+	   console.log(e.target.error);
+	   reject(e.target.error.name); 
 	}
 
 
 	static _createStores(connection) {
 		stores.forEach(store => {
 			if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
-
-			connection.createObjectStore(store, {
-				autoIncrement: true
-			});
-
+            connection.createObjectStore(store, { autoIncrement: true });
 
 		})
 
 
 	}
-}
+	
+	static closeConnection(){
+		close();
+		connection = null;
+	 }
+   }
+ }
+})();
 
 //ConnectionFactory.getConnection();
+
+
