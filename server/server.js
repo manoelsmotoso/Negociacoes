d30a2a9cf6c45ddbf2ab5ea8547d6fad45e95cf5
MANOEL SILVA MOTOSO <manoel.s.motoso@gmail.com>
2018-10-29 11:36:11 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 var http = require('http')
     ,app = require('./config/express');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || 80
+var server_ip_address = process.env.IP || process.env.HOST || '0.0.0.0'
 
 http.createServer(app).listen(server_port, server_ip_address, function() {
     console.log('Servidor estutando em: \n'+server_ip_address+':'+server_port);
